7988165b7aaad71ef31321ad20a20c6885034cbf
Jay Batavia <bataviajay@gmail.com>
2015-07-14 16:37:02 -0600

    Broken Cloud storage

--- Source/Prototype/assets/js/main.js
+++ Source/Prototype/assets/js/main.js
@@ -1,42 +1,103 @@
-	var APP_KEY = "rwovrjf7g18ya3b";
+	var CLIENT_ID = '609410117719-2cpaht834f4kjbaivkt2v1s03to7s9r8.apps.googleusercontent.com';
+	var SCOPES = ["https://www.googleapis.com/auth/drive"];
 
-	document.getElementById("submit").onclick = readFile;
+
+	//document.getElementById("submit").onclick = readFile;
 	var fileName="";
+	//document.getElementById("gdrive_connect").onclick = handleAuthClick;
+	
+	//document.getElementById("getPublink").onclick = getPubLink;
+	document.getElementById("keyGenerate").onclick = initializeKeys;
+	//document.getElementById("decrypt_file").onclick = makeDownloadRequest;
+	//document.getElementById("download_key").onclick = downloadKey;
 
-	document.getElementById("dropbox_connect").onclick = getOAuth;
+	var access_token = "";
+	var publink = null;
+	var file_blob = null;
+	var encryptedPrivKey = null;
+	var sessionSalt = null;
+	var pbkdSalt = null;
+	var filename=null;
+	var sessionKey = null;
+	const boundary = '-------314159265358979323846';
+	const delimiter = "\r\n--" + boundary + "\r\n";
+ 	const close_delim = "\r\n--" + boundary + "--";
 
-	document.getElementById("decrypt_file").onclick = makeDownloadRequest;
+	window.onload = checkAuth;
 
-	var decrypted_text = document.getElementById("dropbox_file");
 
-	function get_redirect_uri() {
-		return window.location.href.substring(0, window.location.href.length - window.location.hash.length).replace(/\/$/, '');
+	function checkAuth(){
+		gapi.auth.authorize(
+		{
+			'client_id': CLIENT_ID,
+			'scope': SCOPES,
+			'immediate': true
+		}, handleAuthResult);
 	}
 
-
-	function getOAuth(e){
-		window.open('https://www.dropbox.com/1/oauth2/authorize?client_id='+encodeURIComponent(APP_KEY)+'&response_type=token&redirect_uri='+encodeURIComponent(get_redirect_uri()), "_self");
-	}
-
-	function parseqs(text) {
-		var split = text.split('&');
-		var params = {};
-		for (var i = 0; i < split.length; i++) {
-			var kv = split[i].split('=', 2);
-			params[kv[0]] = kv[1];
-		}
-		return params;
-	}
-
-	if(window.location.hash){
-		var params = parseqs(window.location.hash.substring(1));
-		if(params.error){
-			document.write("Error "+params.error+": "+params.error_description.replace(/\+/g,' '));
+	function handleAuthResult(authResult){
+		var authDiv = document.getElementById('authorize-div');
+		if(authResult && !authResult.error){
+			authDiv.style.display='none';
+			loadDriveApi();
 		}
 		else{
-			access_token = parseqs(window.location.hash.substring(1)).access_token;
+			authDiv.style.display='inline';
 		}
-	}	
+	}
+
+
+	function handleAuthClick(e){
+		gapi.auth.authorize(
+        	{client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+        	handleAuthResult);
+        return false;
+	}
+
+	function loadDriveApi(){
+		gapi.client.load('drive','v2')
+	}
+
+	function listFiles(){
+		var request = gapi.client.drive.files.list({
+			'maxResults':10
+		});
+
+		request.execute(function(resp){
+			appendPre('Files:');
+			var files = resp.items;
+			if(files && files.length>0){
+				for(var i=0; i<files.length; i++){
+					var file = files[i];
+					appendPre(file.title+' ('+file.id+')');
+				}
+			}
+			else{
+				appendPre('No files found.');
+			}
+		});
+	}
+
+	function appendPre(message){
+		var pre = document.getElementById('output');
+		var textContent = document.createTextNode(message+'\n');
+		pre.appendChild(textContent);
+	}
+
+	// if(window.location.hash){
+
+	// 	var params = parseqs(window.location.hash.substring(1));
+	// 	if(params.error){
+	// 		document.write("Error "+params.error+": "+params.error_description.replace(/\+/g,' '));
+	// 	}
+	// 	else{
+	// 		access_token = parseqs(window.location.hash.substring(1)).access_token;
+	// 		uid = parseqs(window.location.hash.substring(1)).uid;
+	// 		window.location.hash = "";
+	// 		localStorage.setItem('access_token', access_token);
+	// 		sessionStorage.setItem('uid', uid);
+	// 	}
+	// }	
 	
 	function readFile(e){
 		var contents = "";
@@ -52,29 +113,84 @@
 			contents = reader.result;
 			encryptFile(contents);
 		}
-		fileName = file_encrypt.name + ".encrypted";
+		fileName = file_encrypt.name;
 		reader.readAsBinaryString(input_file.files[0]);
 
 	}
 
 	function encryptFile(contents){
 		var encrypted_data = "";
-		var pass = document.getElementById("myPsw").value;
-		var pass2 = document.getElementById("myPsw2").value;
-		if(pass.length<3){
-			alert("Please select a longer password.");
-			return false;
-		}
-		encrypted_data = sjcl.encrypt(pass, contents);
+		sessionKey = generateSessionKey();
+
+
+		encrypted_data = sjcl.encrypt(sessionKey, contents);
 		contents = "";
 		pass="";
-		makeUploadRequest(encrypted_data);
+		fileUploadRequest(encrypted_data, fileName);
 		encrypted_data = "";
-		//obj.innerHTML = encrypted_data;
+	}
+
+	function initializeKeys(){
+		var pass = document.getElementById("myPsw").value;
+
+		var rsa = forge.pki.rsa;
+		
+		var keypair = rsa.generateKeyPair({bits: 2048, e: 0x10001});
+		
+		var pem = forge.pki.publicKeyToPem(keypair.publicKey);
+		
+		var salt1 = forge.random.getBytesSync(128);
+
+		var salt2 = forge.random.getBytesSync(128);
+
+		var pbkd = forge.pkcs5.pbkdf2(pass, salt2, 40, 16);
+
+		var privKey = forge.pki.encryptRsaPrivateKey(keypair.privateKey, pbkd);
+
+		keyUploadRequest(salt1, "salt1");
+		// keyUploadRequest(salt2, "salt2");
+		// keyUploadRequest(pem, "pubKey");
+		// keyUploadRequest(privKey, "privKey");
 	}
 
 
-	function makeUploadRequest(requestData){
+	function keyUploadRequest(requestData, filename, callback){
+		var metadata = {
+			'title': filename
+		};
+
+		var uploadRequest = delimiter+ JSON.stringify(metadata)+delimiter+requestData+close_delim;
+
+		var request = gapi.client.request({
+			'path': '/upload/drive/v2/files',
+			'method':'POST',
+			'params':{'uploadType':'multipart'},
+			'body':uploadRequest
+		});
+		
+		if(!callback){
+			callback = function(file){
+				console.log(file)
+			};
+		}
+		request.execute(callback);
+	}
+
+	function generateSessionKey(){
+		var pswd = document.getElementById("myPsw").value;
+		var sessionPbkd = forge.pkcs5.pbkdf2(pswd, pbkdSalt, 40, 16);
+
+		var uEncryptedPrivKey = forge.pki.decryptRsaPrivateKey(encryptedPrivKey, sessionPbkd);
+		
+		var sessionkey = forge.pkcs5.pbkdf2(sessionSalt, uEncryptedPrivKey, 40, 16);
+
+		return sessionkey;
+
+	}
+
+	//Actual data will go to a path depending on the team. Not to the root folder.
+	function fileUploadRequest(requestData, filename){
+		var teamName = "";
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function(){
 			if(xmlhttp.readyState == 4 && xmlhttp.status==200){
@@ -82,25 +198,69 @@
 			}
 		}
 
-		xmlhttp.open("POST", "https://api-content.dropbox.com/1/files_put/auto/"+fileName+"?overwrite=true", true);
+		xmlhttp.open("POST", "https://api-content.dropbox.com/1/files_put/auto/"+teamName+"/"+filename+"?overwrite=true", true);
 		xmlhttp.setRequestHeader("Authorization"," Bearer "+access_token);
 		xmlhttp.send(requestData);
 	}
 
-	function makeDownloadRequest(e){
+	function downloadKey(){
+		console.log("downloading key");
+		makeDownloadRequest("privKey", keyCallBack);
+		makeDownloadRequest("salt1", salt1CallBack);
+		makeDownloadRequest("salt2", salt2CallBack);
+	}
+
+	function getPubLink(){
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function(){
-			if(xmlhttp.readyState == 4 && xmlhttp.status==200){
-				alert("success!");
-				var file_blob = xmlhttp.responseText;
-				decryptFile(file_blob);
-			}
-		}
-		
-		xmlhttp.open("GET", "https://api-content.dropbox.com/1/files/auto/"+"TEST.encrypted", true);
+
+		xmlhttp.onreadystatechange = makeFooCB(publinkCallBack);
+
+		xmlhttp.open("POST", "https://api.dropbox.com/1/shares/auto/"+"salt1",true);
 		xmlhttp.setRequestHeader("Authorization"," Bearer "+access_token);
 		xmlhttp.send(null);
 	}
+
+
+	function makeDownloadRequest(filename1, cb){
+		var xmlhttp = new XMLHttpRequest();
+		//filename = filename1;
+		xmlhttp.onreadystatechange = makeFooCB(cb);
+	
+		xmlhttp.open("GET", "https://api-content.dropbox.com/1/files/auto/"+filename1+"", true);
+		xmlhttp.setRequestHeader("Authorization"," Bearer "+access_token);
+		xmlhttp.send(null);
+
+	}
+
+	function makeFooCB(cb){
+		return function(ev){
+			foo(this, cb);
+		}
+	}
+
+	function keyCallBack(httpResponse){
+		encryptedPrivKey = httpResponse;
+	}
+
+	function publinkCallBack(httpResponse){
+		publink = httpResponse;
+		console.log(publink);
+	}
+
+	function salt2CallBack(httpResponse){
+		pbkdSalt = httpResponse;
+	}
+
+	function salt1CallBack(httpResponse){
+		sessionSalt = httpResponse;
+	}
+
+	function foo(xhr, cb){
+		if(xhr.readyState == 4 && xhr.status==200){
+			cb(xhr.responseText);
+		}
+	}
+
 
 	function decryptFile(fileBlob){
 		var decrypt_pass = document.getElementById("myPsw2").value;
--- Source/Prototype/testServer.js
+++ Source/Prototype/testServer.js
@@ -1,34 +1,107 @@
-var sys = require("sys"),
-my_http = require("http"),
-path = require("path"),
-url = require("url"),
-filesys = require("fs");
-my_http.createServer(function(request,response){
-    var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(),my_path);
-    console.log(process.cwd());
-    filesys.exists(full_path,function(exists){
-        if(!exists){
-            response.writeHeader(404, {"Content-Type": "text/plain"});  
-            response.write("404 Not Found\n");  
-            response.end();
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+var qs = require('querystring')
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('ManyHands');
+
+var username = "";
+var serve = serveStatic('.', {"index": ['index.html','index.htm']})
+
+
+var server = http.createServer(function (req, res){
+
+    if(req.method=="POST"){
+        var body="";
+        req.on('data', function (chunk){
+            body += chunk.toString();
+        });
+
+        req.on('end', function(){
+            var post = qs.parse(body);
+            res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+            res.end();
+            postReqCB(post);
+        });
+    }
+    else{
+        var done = finalhandler(req, res)
+        serve(req, res, done)
+    }
+});
+
+server.listen(8080);
+
+console.log("Server Running on 8080");
+
+function postReqCB(body){
+    if(body['username']){
+        username = body['username'];
+        checkUser(username);
+    }
+    else{
+        console.log("No username");
+    }
+}
+
+function checkUser(username){
+    db.all("SELECT * from user where uid="+username,function(err, rows){
+        if(err) console.log(err.errno);
+        else if(rows.length === 0){
+            console.log("user does not exist. connect to dropbox");
         }
         else{
-            filesys.readFile(full_path, "binary", function(err, file) {  
-                 if(err) {  
-                     response.writeHeader(500, {"Content-Type": "text/plain"});  
-                     response.write(err + "\n");  
-                     response.end();  
-                
-                 }  
-                 else{
-                    response.writeHeader(200);  
-                    response.write(file, "binary");  
-                    response.end();
-                }
-                      
-            });
+            console.log("user exists");
         }
-    });
-}).listen(8080);
-sys.puts("Server Running on 8080"); 
+    })
+
+}
+
+// var sys = require("sys"),
+// my_http = require("http"),
+// path = require("path"),
+// url = require("url"),
+// filesys = require("fs");
+// serveStatic = require('serve-static')
+
+// my_http.createServer(function(request,response){
+//     var my_url = url.parse(request.url);
+//     var my_path = my_url.pathname;
+//     var full_path = path.join(process.cwd(),my_path);
+
+//     if(my_path == "/getURL"){
+//         getURL(response, my_url);
+//     }
+
+//     request.on("data", function(data){
+//         response.end('data event: '+data);
+//     });
+
+
+
+//     filesys.exists(full_path,function(exists){
+//         if(!exists){
+//             response.writeHeader(404, {"Content-Type": "text/plain"});  
+//             response.write("404 Not Found\n");  
+//             response.end();
+//         }
+//         else{
+//             filesys.readFile(full_path, "binary", function(err, file) {  
+//                  if(err) {  
+//                      response.writeHeader(500, {"Content-Type": "text/plain"});  
+//                      response.write(err + "\n");  
+//                      response.end();  
+                 
+//                  }  
+//                  else{
+//                     response.writeHeader(200);  
+//                     response.write(file, "binary");  
+//                     response.end();
+//                 }
+                      
+//             });
+//         }
+//     });
+// }).listen(8080);
+
+
