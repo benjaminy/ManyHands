e7fb75b32f85e31f266e1ff827d0ec3e4d8404b4
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2016-06-30 10:16:30 -0600

    Added HTTPS support

--- Source/simple_file_server.js
+++ Source/simple_file_server.js
@@ -2,17 +2,25 @@
  * This is an extremely simple HTTP file server
  */
 
+var DEFAULT_PORT = 8080;
+var DEFAULT_DIR  = '.';
+
 var serveStatic  = require( 'serve-static' );
 var http         = require( 'http' );
+var https        = require( 'https' );
 var finalhandler = require( 'finalhandler' );
 var url          = require( 'url' );
 var fs           = require( 'fs' );
 var path         = require( 'path' );
 var mkdirp       = require( 'mkdirp' );
+var opt          = require( 'node-getopt' ).create( [
+    [ 'P', 'port=PORT',      'Port to server on; default is '+DEFAULT_PORT ],
+    [ 'D', 'dir=DIR',        'Root directory to serve from; default is '+DEFAULT_DIR ],
+    [ 'C', 'cert=CERT_FILE', 'Certificate file; will serve over HTTP if not given' ],
+    [ 'K', 'key=KEY_FILE',   'key file; will serve over HTTP if not given' ] ] )
+  .bindHelp().parseSystem();
 
-var DEFAULT_PORT = 8080;
-var DEFAULT_DIR  = '.';
-var root_dir     = null;
+var root_dir = DEFAULT_DIR;
 
 var CORS_HEADERS = [ ['Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,DELETE'],
                      [ 'Access-Control-Allow-Origin', '*' ],
@@ -133,45 +141,43 @@ function handleDynamic( req, resp )
 
 function runServer()
 {
-    var p = parseArgs();
+    console.log( opt.options );
+    if( 'dir' in opt.options )
+        root_dir = opt.options.dir;
+    var port = DEFAULT_PORT;
+    if( 'port' in opt.options )
+    {
+        var x = parseInt( opt.options.port );
+        if( !isNaN( x ) )
+            port = x;
+    }
 
     var serveFiles = serveStatic( root_dir, { 'index': [ 'index.html', 'index.htm' ] } );
-    // var serveFiles = serveStatic( root_dir, { 'index': false } );
 
-    var server = http.createServer(
-        function( req, resp ) {
-            for( i = 0; i < CORS_HEADERS.length; i++ )
-            {
-                resp.setHeader( CORS_HEADERS[i][0], CORS_HEADERS[i][1] );
-            }
-            serveFiles( req, resp, function() { handleDynamic( req, resp ) } );
-        } );
-    server.listen( p );
-    log( 'Simple File Server: Serving directory', root_dir, 'on port', p );
-}
-
-function parseArgs()
-{
-    var p = null;
-    for( i = 2; i < process.argv.length; i++ )
-    {
-        var x = parseInt( process.argv[i] );
-        if( isNaN( x ) )
+    function handleReq( req, resp ) {
+        for( i = 0; i < CORS_HEADERS.length; i++ )
         {
-            if( !root_dir )
-                root_dir = process.argv[i];
+            resp.setHeader( CORS_HEADERS[i][0], CORS_HEADERS[i][1] );
         }
-        else
-        {
-            if( !p )
-                p = x;
-        }
+        serveFiles( req, resp, function() { handleDynamic( req, resp ) } );
     }
-    if( !root_dir )
-        root_dir = DEFAULT_DIR;
-    if( !p )
-        p = DEFAULT_PORT;
-    return p;
+
+    var server, protocol = 'HTTP';
+    if( 'cert' in opt.options && 'key' in opt.options )
+    {
+        var options = {
+            key:  fs.readFileSync( opt.options.key ),
+            cert: fs.readFileSync( opt.options.cert )
+        };
+        server = https.createServer( options, handleReq );
+        protocol = 'HTTPS'
+    }
+    else
+    {
+        server = http.createServer( handleReq );
+    }
+    server.listen( port );
+    log( 'Simple File Server: Serving directory', root_dir, 'on port', port, 'with protocol', protocol );
 }
 
 runServer();
