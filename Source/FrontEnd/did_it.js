199a83175e9061f3652cf760f11eb2cb3680ebd1
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2017-07-14 10:15:44 -0600

    Stashing frontend stuff in a different branch

---
+++ Source/FrontEnd/did_it.js
@@ -0,0 +1,26 @@
+
+
+function onLoad()
+{
+    // "Loading"
+    var entId = 42; // XXX load entity ID of Did It from local storage
+    var datoms = query( "entid = 42" );
+    var teammates = []
+    var jobs = []
+    var other = []
+    for( var i = 0; i < datoms.length; i++ )
+    {
+        if( datoms[ i ].attribute == "did_it:teammate" )
+            teammates.push( datoms[ i ] );
+        else if( datoms[ i ].attribute == "did_it:job" )
+            jobs.push( datoms[ i ] );
+        else
+            other.push( datoms[ i ] );
+    }
+    for( var i = 0; i < teammates.length; i++ )
+    {
+        add teammate
+    }
+    add date
+    add notes
+}
