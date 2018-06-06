961cf2d663c3ebde4fbcc36740bd2691541eb28d
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2018-06-06 08:46:52 -0600

    more playing with keys

--- Client/Testing/playing_with_web_crypto.mjs
+++ Client/Testing/playing_with_web_crypto.mjs
@@ -32,17 +32,28 @@ async function main()
         "jwk", keyOps( k_exp_dsa_pri, [ "deriveKey", "deriveBits" ] ),
         dh, true, [ "deriveKey", "deriveBits" ] );
 
-    const bits_der = await WCS.deriveBits(
-        Object.assign( {}, dh, { public: k_imp_dh_pub } ), k_imp_dh_pri, 256 );
-    const k_kdf_imp = await WCS.importKey(
-        "raw", bits_der, { name: "PBKDF2", }, false, [ "deriveKey", "deriveBits" ] );
+    const bits_ig = await WCS.deriveBits(
+        Object.assign( {}, dh, { public: k_imp_dh_pub } ), k_gen_dh.privateKey, 256 );
+    const bits_gi = await WCS.deriveBits(
+        Object.assign( {}, dh, { public: k_gen_dh.publicKey } ), k_imp_dh_pri, 256 );
+
+    const k_kdf_ig_imp = await WCS.importKey(
+        "raw", bits_ig, { name: "PBKDF2", }, false, [ "deriveKey", "deriveBits" ] );
+    const k_kdf_gi_imp = await WCS.importKey(
+        "raw", bits_gi, { name: "PBKDF2", }, false, [ "deriveKey", "deriveBits" ] );
+
     const salt = WC.getRandomValues( new Uint8Array( 16 ) );
-    const k_kdf = await WCS.deriveKey(
+    const k_kdf_ig = await WCS.deriveKey(
         { "name": "PBKDF2", salt: salt, iterations: 1, hash: {name: "SHA-1"}, },
-        k_kdf_imp,
-        { name: "AES-CBC", length: 256, },
-        false,
-        [ "encrypt", "decrypt" ] );
+        k_kdf_ig_imp,
+        { name: "AES-CBC", length: 256, }, true, [ "encrypt", "decrypt" ] );
+    const k_kdf_gi = await WCS.deriveKey(
+        { "name": "PBKDF2", salt: salt, iterations: 1, hash: {name: "SHA-1"}, },
+        k_kdf_gi_imp,
+        { name: "AES-CBC", length: 256, }, true, [ "encrypt", "decrypt" ] );
+
+    const k_exp_kdf_ig = await WCS.exportKey( "jwk", k_kdf_ig );
+    const k_exp_kdf_gi = await WCS.exportKey( "jwk", k_kdf_gi );
 
     console.log( "GEN DSA",     k_gen_dsa );
     console.log( "GEN DH",      k_gen_dh );
@@ -54,8 +65,8 @@ async function main()
     console.log( "IMP DSA PRI", k_imp_dsa_pri );
     console.log( "IMP DH PUB",  k_imp_dh_pub );
     console.log( "IMP DH PRI",  k_imp_dh_pri );
-    console.log( "BITS",        bits_der );
-    console.log( "KDF",         k_kdf );
+    console.log( "KDF IG",      k_exp_kdf_ig );
+    console.log( "KDF GI",      k_exp_kdf_gi );
 }
 
 main();
