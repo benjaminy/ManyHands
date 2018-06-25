a0c169450e5cd7cd5486703ff2bafe0097368d74
Beau Carlborg <bcarlborg@gmail.com>
2018-06-24 21:12:15 -0600

    well tested crypto wrappers

---
+++ Client/Source/Crypto/beau/clean/crypto_wrappers.mjs
@@ -0,0 +1,231 @@
+import assert from "assert";
+import {text_to_array_buffer} from "./util.mjs";
+
+import WebCrypto from "node-webcrypto-ossl";
+const WC = new WebCrypto();
+const CS = WC.subtle;
+
+import TextEncoder from "text-encoding";
+const Encoder = new TextEncoder.TextEncoder();
+const Decoder = new TextEncoder.TextDecoder('utf-8');
+
+let IV_VAL = new Uint32Array([ 4183535103, 1663654904, 3786963079, 1962665393 ]).buffer;
+
+export async function form_message_buffer(secret_key, header, message_text) {
+    assert(new DataView(secret_key).byteLength === 32);
+    assert(typeof message_text === "string");
+    assert(typeof header === "object");
+
+    let encryption = await encrypt_text(secret_key, message_text);
+
+    let header_buffer = encode_object(header);
+    let header_length = new DataView(header_buffer).byteLength;
+    let header_length_buffer = new Uint32Array([header_length]).buffer;
+
+
+    let data_to_sign = combine_buffers([header_length_buffer, header_buffer, encryption]);
+    let signature = await sign(secret_key, data_to_sign);
+
+    let output_buffer = combine_buffers([signature, data_to_sign]);
+
+    return output_buffer;
+}
+
+
+export async function parse_message_buffer(secret_key, input_buffer) {
+    assert(new DataView(secret_key).byteLength === 32);
+    assert(new DataView(input_buffer).byteLength >= 36);
+
+    let input_typed_array = new Uint8Array(input_buffer);
+    let input_length = input_typed_array.length;
+
+    let signature = input_typed_array.slice(0, 32).buffer
+    let signed_data = input_typed_array.slice(32, input_length).buffer
+    await verify(secret_key, signature, signed_data);
+
+    let header_length = new Uint32Array(input_typed_array.slice(32, 36))[0];
+    let header_buffer = input_typed_array.slice(36, (36 + header_length)).buffer;
+    let header = decode_object(header_buffer);
+
+    let cipher_text = input_typed_array.slice((36 + header_length), input_length).buffer
+    let plain_text = await decrypt_text(secret_key, cipher_text);
+
+    return { "header": header, "plain_text": plain_text }
+}
+
+export async function encrypt_text(secret_key, plain_text) {
+    assert(typeof plain_text === "string");
+    assert(new DataView(secret_key).byteLength === 32);
+
+    let text_buffer = encode_string(plain_text);
+
+    let cbc_key = await CS.importKey(
+        "raw", secret_key, { name: "AES-CBC" }, false, ["encrypt", "decrypt"]
+    );
+
+    let message_encryption = await CS.encrypt(
+        { name: "AES-CBC", iv: IV_VAL }, cbc_key, text_buffer
+    );
+
+    return message_encryption;
+}
+
+
+export async function decrypt_text(secret_key, cipher_text) {
+    assert(new DataView(secret_key).byteLength === 32);
+    assert(new DataView(cipher_text).byteLength > 0);
+
+    let cbc_key = await CS.importKey(
+        "raw", secret_key, { name: "AES-CBC" }, false, ["encrypt", "decrypt"]
+    );
+
+    let decryption_buffer = await CS.decrypt(
+        { name: "AES-CBC", iv: IV_VAL }, cbc_key, cipher_text
+    );
+
+    let decryption_text = decode_string(decryption_buffer);
+
+    return decryption_text;
+}
+
+export async function sign(secret_key, data_to_sign) {
+    assert(new DataView(secret_key).byteLength === 32);
+    assert(new DataView(data_to_sign).byteLength > 0);
+
+    let sign_key = await CS.importKey(
+        "raw", secret_key, { name: "HMAC", hash: {name: "SHA-256"} }, false, ["sign", "verify"]
+    );
+
+    let signature = await CS.sign(
+        { name: "HMAC" }, sign_key, data_to_sign
+    );
+
+    return signature;
+}
+
+export async function verify(secret_key, signature, signed_data) {
+    assert(new DataView(secret_key).byteLength === 32);
+    assert(new DataView(signed_data).byteLength > 0);
+    assert(new DataView(signature).byteLength === 32);
+
+    let sign_key = await CS.importKey(
+        "raw", secret_key, { name: "HMAC", hash: {name: "SHA-256"} }, false, ["sign", "verify"]
+    );
+
+    let verification = await CS.verify(
+        { name: "HMAC" }, sign_key, signature, signed_data
+    );
+
+    assert(verification);
+    return verification;
+}
+
+export async function root_kdf_step(root_key, ratchet_seed) {
+    assert(new DataView(root_key).byteLength === 32);
+    assert(new DataView(ratchet_seed).byteLength === 32);
+
+    let kdf_input = combine_buffers([root_key, ratchet_seed]);
+
+    let kdf_key = await CS.importKey(
+        "raw", kdf_input, { name: "PBKDF2" }, false, ["deriveKey", "deriveBits"]
+    );
+
+    let output = await CS.deriveBits(
+        { "name": "PBKDF2", salt: IV_VAL, iterations: 1000, hash: {name: "SHA-1"} },
+         kdf_key, 256
+    );
+
+    return {root_key: output, chain_key: output};
+
+}
+
+export async function chain_kdf_step(chain_key) {
+    assert(new DataView(chain_key).byteLength === 32);
+
+    let kdf_key = await CS.importKey(
+        "raw", ck, { name: "PBKDF2" }, false, ["deriveKey", "deriveBits"]
+    );
+
+    let output = await CS.deriveBits(
+        { "name": "PBKDF2", salt: IV_VAL, iterations: 1000, hash: {name: "SHA-1"} },
+         kdf_key, 256
+    );
+
+    return {chain_key: output, message_key: output};
+}
+
+
+export async function generate_dh_key() {
+    let keypair = await CS.generateKey(
+        { name: "ECDH", namedCurve: "P-256"},
+        true, ["deriveKey", "deriveBits"]
+    );
+
+    return keypair;
+}
+
+export async function export_dh_key(key) {
+    let key_object = await CS.exportKey( "jwk", key );
+    return key_object;
+}
+
+export async function import_dh_key(key_object) {
+    let imported_key = await CS.importKey(
+        "jwk", key_object, { name: "ECDH", namedCurve: "P-256" },
+        true, ["deriveKey", "deriveBits"]
+    );
+
+    return imported_key;
+}
+
+export async function random_secret() {
+    let typed_array = await WC.getRandomValues(new Uint8Array(32))
+    let typed_buffer = typed_array.buffer;
+    return typed_buffer;
+}
+
+export function encode_string(text) {
+    let typed_array = Encoder.encode(text);
+    let buffer = typed_array.buffer;
+    return buffer;
+}
+
+export function decode_string(buffer) {
+    let text = Decoder.decode(buffer);
+    return text;
+}
+
+export function encode_object(input_object) {
+    let object_string = JSON.stringify(input_object);
+    let object_buffer = encode_string(object_string);
+    return object_buffer;
+}
+
+export function decode_object(input_buffer) {
+    let object_string = decode_string(input_buffer);
+    let output_object = JSON.parse(object_string);
+    return output_object;
+}
+
+export function combine_buffers(buffer_array) {
+    assert(Array.isArray(buffer_array));
+    let total_number_of_bytes = 0;
+
+    for (let i = 0; i < buffer_array.length; i++) {
+        let curr = buffer_array[i];
+        assert(new DataView(curr).byteLength > 0);
+        buffer_array[i] = new Uint8Array(buffer_array[i]);
+        total_number_of_bytes += buffer_array[i].length;
+    }
+
+    let output = new Uint8Array(total_number_of_bytes);
+    let offset = 0;
+
+    for (let i = 0; i < buffer_array.length; i++) {
+        output.set(buffer_array[i], offset);
+        offset += buffer_array[i].length;
+    }
+
+    return output.buffer;
+
+}
---
+++ Client/Source/Crypto/beau/test/test_crypto_wrappers.mjs
@@ -0,0 +1,157 @@
+import assert from "assert";
+import * as my_crypto from "../clean/crypto_wrappers";
+
+import WebCrypto from "node-webcrypto-ossl";
+const WC = new WebCrypto();
+const CS = WC.subtle;
+
+import TextEncoder from "text-encoding";
+const Encoder = new TextEncoder.TextEncoder();
+const Decoder = new TextEncoder.TextDecoder('utf-8');
+
+async function main() {
+    await test_form_and_parse_message_with_empty_header();
+    await test_form_and_parse_message();
+    await test_encrypt_decrypt();
+    await test_sign_verify();
+    await test_encode_decode_string();
+    await test_encode_decode_object();
+    await test_combine_buffers();
+    await test_export_import_dh_public_key();
+}
+main();
+
+async function test_form_and_parse_message() {
+    named_log("test forming and parsing a complete message");
+
+    let shared_secret = await my_crypto.random_secret();
+    let initial_message = "I'm running away to the circus mom!";
+    let initial_header = { "last_name": "ever", "first_name": "greatest" }
+
+    let message_buffer = await my_crypto.form_message_buffer(
+        shared_secret, initial_header, initial_message
+    );
+    let parsed_messaage = await my_crypto.parse_message_buffer(shared_secret, message_buffer);
+
+    assert(parsed_messaage.plain_text === initial_message);
+    assert(parsed_messaage.header.last_name === initial_header.last_name);
+    assert(parsed_messaage.header.first_name === initial_header.first_name);
+    success();
+}
+
+async function test_form_and_parse_message_with_empty_header() {
+    named_log("test forming and parsing a complete message");
+
+    let shared_secret = await my_crypto.random_secret();
+    let initial_message = "I'm running away to the circus mom!";
+    let initial_header = {};
+
+    let message_buffer = await my_crypto.form_message_buffer(
+        shared_secret, initial_header, initial_message
+    );
+    let parsed_messaage = await my_crypto.parse_message_buffer(shared_secret, message_buffer);
+
+    assert(parsed_messaage.plain_text === initial_message);
+    assert(Object.keys(parsed_messaage.header).length === 0);
+    success();
+}
+
+async function test_encrypt_decrypt() {
+    named_log("test encrypting and decrypting a message");
+
+    let shared_secret = await my_crypto.random_secret();
+    let initial_message = "I'm running away to the circus mom!";
+
+    let cipher_text = await my_crypto.encrypt_text(shared_secret, initial_message);
+    let decryption = await my_crypto.decrypt_text(shared_secret, cipher_text);
+    assert(initial_message === decryption);
+    success();
+}
+
+async function test_sign_verify() {
+    named_log("test signing and verifying a message");
+
+    let shared_secret = await my_crypto.random_secret();
+    let initial_message = "I'm running away to the circus mom!";
+
+    let initial_message_buffer = my_crypto.encode_string(initial_message);
+
+    let signature = await my_crypto.sign(shared_secret, initial_message_buffer);
+    let verification = await my_crypto.verify(shared_secret, signature, initial_message_buffer);
+    assert(verification);
+    success();
+}
+
+async function test_encode_decode_string() {
+    named_log("test encoding and decoding a string");
+    let string_to_encode = "Foo Bar Baz";
+
+    let encoded_string = my_crypto.encode_string(string_to_encode);
+    let decoded_string = my_crypto.decode_string(encoded_string);
+
+
+    assert(string_to_encode === decoded_string);
+    success();
+}
+
+async function test_encode_decode_object() {
+    named_log("test encoding and decoding an object");
+    let my_object = {
+        "pet": "cat",
+        "age": 10,
+        "carreer": "winner"
+    };
+
+    let encoded_object = my_crypto.encode_object(my_object);
+    let decoded_object = my_crypto.decode_object(encoded_object);
+    assert(decoded_object.pet === my_object.pet);
+    assert(decoded_object.age === my_object.age);
+    assert(decoded_object.carreer === my_object.carreer);
+    success();
+}
+
+async function test_export_import_dh_public_key() {
+    named_log("test import and export a public key");
+    let dh_keypair = await my_crypto.generate_dh_key();
+    let public_key = dh_keypair.publicKey;
+
+    let key_object = await my_crypto.export_dh_key(public_key);
+    let imported_key = await my_crypto.import_dh_key(key_object);
+
+    let re_exported = await CS.exportKey("jwk", imported_key);
+    
+    assert(key_object.kty === re_exported.kty);
+    assert(key_object.crv === re_exported.crv);
+    assert(key_object.x === re_exported.x);
+    assert(key_object.y === re_exported.y);
+    success();
+}
+
+async function test_combine_buffers() {
+    named_log("test combine buffer arrays");
+
+    let string_one = "the lazy brown fox";
+    let string_two = " jumps over the log";
+
+    let buffer_one = my_crypto.encode_string(string_one);
+    let buffer_two = my_crypto.encode_string(string_two);
+
+    let combined_buffers = my_crypto.combine_buffers([buffer_one, buffer_two]);
+    let decoded_combined_buffers = my_crypto.decode_string(combined_buffers)
+
+    assert((string_one + string_two) === decoded_combined_buffers);
+    success();
+}
+
+function named_log(name) {
+    let cyan = '\x1b[36m%s\x1b[0m';
+    console.log(cyan, "*******************************");
+    console.log(cyan, name);
+    console.log(cyan, "*******************************");
+}
+
+function success() {
+    let green = "\x1b[32m%s\x1b[0m";
+    console.log(green, "success!");
+    console.log();
+}
