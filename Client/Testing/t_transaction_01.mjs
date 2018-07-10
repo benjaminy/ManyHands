82b35b25ba13178b9ed5b4455587b3d9fbb84e36
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2018-07-10 12:25:02 -0600

    data

--- Client/Testing/t_transaction_01.mjs
+++ Client/Testing/t_transaction_01.mjs
@@ -1,33 +1,35 @@
 /* Top Matter */
 
+import Path from "path";
 import assert  from "../Source/Utilities/assert";
-import Path    from "path";
 import * as K  from "../Source/Utilities/keyword";
 import * as DA from "../Source/Database/attribute";
 import * as DT from "../Source/Database/transaction";
 import * as DB from "../Source/Database/simple_txn_chain";
 import * as DC from "../Source/Database/common";
-import * as MS from "../Source/Storage/in_memory";
+import SM      from "../Source/Storage/in_memory";
 
-// [ DT.addK, "bob", ":age", 42 ]
+// // [ DT.addK, "bob", ":age", 42 ]
 
-// async function main()
-// {
-//     const storage = MS.init( { path_prefix: [ "alice", "DB2" ] } );
-//     const db = DB.newDB( storage, {} );
+async function main()
+{
+    const s = SM( { path_prefix: [ "bob", "misc" ] } );
+    const db = DB.newDB( s );
+// //     const storage = MS.init( { path_prefix: [ "alice", "DB2" ] } );
+// //     const db = DB.newDB( storage, {} );
 
-//     ageAttr = DA.makeAttribute(
-//         ":age", DA.vtypeLong, DA.cardinalityOne,
-//         "doc doc doc" );
-//     likesAttr = DA.makeAttribute(
-//         ":likes", DA.vtypeLong, DA.cardinalityOne,
-//         "doc doc doc" );
+// //     ageAttr = DA.makeAttribute(
+// //         ":age", DA.vtypeLong, DA.cardinalityOne,
+// //         "doc doc doc" );
+// //     likesAttr = DA.makeAttribute(
+// //         ":likes", DA.vtypeLong, DA.cardinalityOne,
+// //         "doc doc doc" );
 
-//     const ageAddStmt   = DA.makeAddTxnStmt( ageAttr );
-//     const likesAddStmt = DA.makeAddTxnStmt( likesAttr );
-//     //db.add
-// }
+// //     const ageAddStmt   = DA.makeAddTxnStmt( ageAttr );
+// //     const likesAddStmt = DA.makeAddTxnStmt( likesAttr );
+// //     //db.add
+}
 
-// main()
+main();
 
-console.log( "Reaching end of t_transaction_02.mjs" );
+console.log( "Reaching end of t_transaction_01.mjs" );
