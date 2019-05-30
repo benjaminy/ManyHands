31a86ab490c5665799e45cc3038ed7d0b58f2871
Daniel Barnes <daniel@danielbarnes.me>
2019-05-30 13:03:01 -0600

    Storage/Dropbox: add instructions for running the tests on Dropbox

--- Client/Source/Storage/Daniel/Dropbox/test_dropbox.mjs
+++ Client/Source/Storage/Daniel/Dropbox/test_dropbox.mjs
@@ -1,5 +1,14 @@
 import dropbox from "./dropbox.mjs";
 
+// TO RUN:
+
+// Register a new app at the following link: https://www.dropbox.com/developers/apps
+// When you have registered an app, click on Settings and find the OAuth2 row
+// Under "Generate Access Token", click "Generate", and then copy the generated token
+// The token provided must be inserted into a new file, 'access.token'
+
+// Once these steps are completed, this file should sample an upload and download of a sample file.
+
 /* call init */
 const storage = dropbox();
 
