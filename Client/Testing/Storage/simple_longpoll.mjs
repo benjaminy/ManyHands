2f4e3d570a759de0a5d878d0c773fab031b1de80
Mataan Peer <mataanpeer@gmail.com>
2019-06-26 12:08:23 -0600

    finished longpoll tests

--- Client/Testing/Storage/simple_longpoll.mjs
+++ Client/Testing/Storage/simple_longpoll.mjs
@@ -1,5 +1,13 @@
 import T       from "transit-js";
 
 export async function longPollTests( s ){
-  link = T.map()
+  const link = T.map();
+  const path = //seomtjing ;
+  link.set("path",`${path}`);
+  const etag = //something ;
+  const options = {
+    etag: etag
+    timeoutLength: 40000
+  }
+  s.watch(link,options);
 }
